7de3c8edfe9877c2c3e58dccc0fb124f57d7bc06
Neel Popat <neelpopat242@gmail.com>
2021-12-25 11:12:41 +0530

    array 2

--- array_2.cpp
+++ array_2.cpp
@@ -1,8 +1,53 @@
 #include<iostream>
+#include<stdlib.h>
+#include<time.h>
 using namespace std;
 
+
+int max(int arr[], int n){
+    int k = arr[0];
+    for (int i=0;i<n;i++){
+        if (arr[i]>k){
+            k=arr[i];
+        }
+    }
+    return k;
+}
+
+int min(int arr[], int n){
+    int k = arr[0];
+    for (int i=0;i<n;i++){
+        if (arr[i]<k){
+            k=arr[i];
+        }
+    }
+    return k;
+}
 int main()
-{
+{   
     
+    srand(time(0));
+    int n=rand()%10; 
+   
+    int a[n] ;
+    
+    
+    for(int i=0;i<n;i++){
+        a[i]=rand()%100;
+    }
+    
+    // cin>>n;
+    // for(int i=0;i<n;i++)
+    // {
+    //     cin>>a[i];
+    // }  
+    int max_num = max(a,n);
+    int min_num = min(a,n);
+    cout<<"Max "<<max_num<<" Min "<<min_num;
+    for(int i=0;i<n;i++)
+    {
+        cout<<"\n"<<a[i]<<" "<<i<<"  ";
+    } 
+
     return 0;
 }
