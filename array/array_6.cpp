a4d16a622b4d409249d30f4bcd39736f615f6b22
Neel Popat <neelpopat242@gmail.com>
2021-12-26 10:19:53 +0530

    array 7 complete

---
+++ array/array_6.cpp
@@ -0,0 +1,55 @@
+//Set func/datatype  is to be used here 
+//Set has a property that the values that are present inside are not repeated and would be present once only.
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template in C++
+
+class Solution{
+    public:
+    //Function to return the count of number of elements in union of two arrays.
+    int doUnion(int a[], int n, int b[], int m)  {
+        //code here
+        set<int> s;
+        int i;
+        for(i=0;i<n;i++){
+            s.insert(a[i]);
+        }
+        for(i=0;i<m;i++){
+            s.insert(b[i]);
+        }
+        return s.size();
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n, m;
+	    cin >> n >> m;
+	    int a[n], b[m];
+	   
+	    for(int i = 0;i<n;i++)
+	       cin >> a[i];
+	       
+	    for(int i = 0;i<m;i++)
+	       cin >> b[i];
+	    Solution ob;
+	    cout << ob.doUnion(a, n, b, m) << endl;
+	    
+	}
+	
+	return 0;
+}  // } Driver Code Ends
